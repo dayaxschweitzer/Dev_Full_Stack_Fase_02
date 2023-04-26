0cec696f568cde239df3ea9c3ad9737b779f578f
Dayane X Schweitzer <110023407+dayaxschweitzer@users.noreply.github.com>
2023-04-26 18:44:36 -0300

    exercício 1_parte front

---
+++ index_odenar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste de Ordenação</title>
+</head>
+<body>
+	<h1>Teste de Ordenação</h1>
+	<form method="post" action="ordenar.php">
+		<label for="arr">Digite os números separados por vírgula:</label>
+		<input type="text" name="arr" id="arr">
+		<button type="submit">Ordenar</button>
+	</form>
+</body>
+</html>
